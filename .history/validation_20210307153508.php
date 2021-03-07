2d7536a05a4bfa4cb066a9180200d1621062d094
yoshiryoryo <19920629ryo@gmail.com>
2021-03-07 15:39:35 +0900

    03/07

--- .history/validation_20210307153508.php
+++ .history/validation_20210307153508.php
@@ -50,16 +50,9 @@ if (isset($_GET)) {
     if (empty($_GET['email'])) {
         $errors[] = '「メールアドレス」は必須項目です。';
     } elseif (!filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) {
-        $errors[] = '正しいメールアドレスを指定してください。';
+        $errors[] = '正しいEメールアドレスを指定してください。';
     }
-
-    // 補足事項
-    if( 100 < mb_strlen($_GET['content'])) {
-        $error[] = "補足事項は100文字以内で入力してください";
-      }
-
-    // セッション時間を10秒
-
+    
 }
 ?>
 <!DOCTYPE html>
@@ -98,38 +91,51 @@ if (isset($_GET)) {
 
 <body>
     <?php if (empty($errors)) : ?>
-        <h1>確認画面</h1>
-        <form action="member.php" method="get">
-            <div>
-                <tr>
-                    <th>姓（セイ）:</th>
-                    <!--htmlタグとして機能しないようにする、クロスサイトスクリプティング対策-->
-                    <td><?= htmlspecialchars($_GET['sei']) ?></td>
-                </tr>
-                <tr>
-                    <th>名（メイ）:</th>
-                    <td><?= htmlspecialchars($_GET['mei']) ?></td>
-                </tr>
-                <tr>
-                    <th>メールアドレス：</th>
-                    <td><?= htmlspecialchars($_GET['email']) ?></td>
-                </tr>
-                <tr>
-                    <th>補足事項：</th>
-                    <!--nl2brはenterに対してbrタグを追加するためのもの-->
-                    <td><?= nl2br(htmlspecialchars($_GET['content'])) ?></td>
-                </tr>
-                <button type="submit" name="confirm" value="send">登録</button>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-offset-4 col-xs-4">
+                    <h1>確認画面</h1>
+                    <form action="member.php" method="get">
+                        <div class="form-group">
+                            <tr>
+                                <th>姓（セイ）:</th>
+                                <!--htmlタグとして機能しないようにする、クロスサイトスクリプティング対策-->
+                                <td><?= htmlspecialchars($_GET['sei']) ?></td>
+                            </tr>
+                        </div>
+                        <div class="form-group">
+                            <tr>
+                                <th>名（メイ）:</th>
+                                <td><?= htmlspecialchars($_GET['mei']) ?></td>
+                            </tr>
+                        </div>
+                        <div class="form-group">
+                            <tr>
+                                <th>メールアドレス：</th>
+                                <td><?= htmlspecialchars($_GET['email']) ?></td>
+                            </tr>
+                        </div>
+                        <div class="form-group">
+                            <tr>
+                                <th>補足事項：</th>
+                                <!--nl2brはenterに対してbrタグを追加するためのもの-->
+                                <td><?= nl2br(htmlspecialchars($_GET['content'])) ?></td>
+                            </tr>
+                        </div>
+                            <button type="submit" name="confirm" class="btn btn-success btn-block">登録</button>
+                        
+                    </form>
+                </div>
             </div>
-        </form>
+        </div>
     <?php else : ?>
         <div class="col-xs-offset-4 col-xs-4">
-        <ul class="error_list">
-            <?php foreach ($errors as $msg) : ?>
-                <li><?= $msg ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+            <ul class="error_list">
+                <?php foreach ($errors as $msg) : ?>
+                    <li><?= $msg ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
         <div class="container">
             <div class="row">
